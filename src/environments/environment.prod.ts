4e5d95e113122b32641780280f0e1ae4a11b750c
CarleoneSouza <carleonedoom@hotmail.com>
2023-12-01 17:44:01 -0300

    fix: remove env file from git track

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: true,
-    apiManager: 'http://localhost:8081/api/v1/',
-    apiDocs: 'http://localhost:8081/api/v1/',
-    recaptcha: {
-        siteKey: '6LffYx8pAAAAAPM-yh55gZ24O8Utjnkz9oKbMBMI',
-      },
-};
